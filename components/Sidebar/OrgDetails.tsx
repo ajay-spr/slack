ad442c4c169c72f47ae7e3145cd70f259d12b17d
Ajay Yadav <ajay.yadav@sprinklr.com>
2023-07-20 23:03:47 +0530

    fix logo color

--- components/Sidebar/OrgDetails.tsx
+++ components/Sidebar/OrgDetails.tsx
@@ -8,7 +8,7 @@ const OrgDetails = () => {
         <span className="font-bold block text-blue-200">Apple Inc.</span>
         <span className="text-blue-100/50">1324 members</span>
       </div>
-      <div className="rounded-[50%] bg-blue-100 p-2 h-10 w-10 flex items-center justify-center border border-blue-300">
+      <div className="rounded-[50%] bg-blue-100 p-2 h-10 w-10 flex items-center justify-center text-blue-950 border border-blue-300">
         <FontAwesomeIcon icon={faPenToSquare} />
       </div>
     </div>
