c98ad28a97338eb433d3e4b9f8bb01fb5275d50d
Ajay Yadav <ajay.yadav@sprinklr.com>
2023-07-21 18:12:35 +0530

    create updateActiveChatId action in useChats hook

--- hooks/useChats.ts
+++ hooks/useChats.ts
@@ -2,6 +2,11 @@ import { ChatMetaData, User } from "@/types";
 import { UDeBruyne, UHaaland, UKane, UMbappe, UMessi, UNeymar, URonaldo, USalah } from "@/utils/generateUsers";
 import { useState } from "react"
 
+export enum ChatActionType {
+    UPDATE_ACTIVE_CHAT_ID
+}
+
+
 const useChats = (userId: string) => {
     const [activeChatId, setActiveChatId] = useState<null | string>(null)
    
@@ -33,7 +38,15 @@ const useChats = (userId: string) => {
         },
       ];
 
-    const onAction = () => {
+    const onAction = (type: ChatActionType, payload: any) => {
+        switch(type) {
+            case ChatActionType.UPDATE_ACTIVE_CHAT_ID:
+                // do here
+                setActiveChatId(payload.id)
+                break
+            default:
+                break
+        }
     }
 
     return {activeChatId, allChats, onAction}
