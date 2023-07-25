0b87262dcb4c7e9352b66123545a0d836ea7022a
Ajay Yadav <ajay.yadav@sprinklr.com>
2023-07-25 23:23:27 +0530

    Add styles to Message and MessageList

--- components/Message/index.tsx
+++ components/Message/index.tsx
@@ -1,15 +1,29 @@
 import { TMessage } from "@/types";
+import { getUserFromId } from "@/utils/generateUsers";
 
 type MessageProps = {
   message: TMessage;
 };
 
 const Message: React.FC<MessageProps> = ({ message }) => {
-  return (
-    <div className="w-full bg-slate-100 rounded px-3 py-2">
-      message: {JSON.stringify(message)} date: {message.timestamp.getDate()}
+  const user = getUserFromId(message.from);
+  const getTimeString = (timestamp: Date) => {
+    return timestamp.toLocaleString();
+  };
+  return message.type == "text" ? (
+    <div className="w-full rounded px-3 py-2 flex gap-2">
+      <div className="bg-slackBlue h-10 w-10 rounded"></div>
+      <div>
+        <div className="flex gap-2 items-baseline">
+          <span className="font-bold">{user?.fullName}</span>
+          <span className="text-sm text-slate-400">
+            {getTimeString(message.timestamp)}
+          </span>
+        </div>
+        <div>{message.message}</div>
+      </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Message;
